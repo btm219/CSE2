d94cb667b192a03affe1fa6a08fab2173da09f9e
btm219 <btm219@lehigh.edu>
2015-09-01 04:19:37 +0000

    added hw01

---
+++ Variables/practice.java
@@ -0,0 +1,17 @@
+public class practice{
+
+//add main method
+public static void main(String[] args)
+{
+    float processorSpeed;
+    byte numberProccesors;
+    float computerSpeed;
+    
+    processorSpeed = 2.3f;
+    numberProccesors = 3;
+    
+    computerSpeed = processorSpeed * numberProccesors;
+    
+    System.out.println("Computer Speed = " + computerSpeed);
+    
+}}
---
+++ hw01/hw01.java
@@ -0,0 +1,29 @@
+//Ben Meyerson
+//homework 1
+//hw01 program
+//
+//compile program first
+//  javac hw01.java
+//run program
+//  java hw01//
+
+//define a class
+public class hw01{
+
+//add a main method 
+public static void main(String[] args) {
+
+System.out.println("########################");
+System.out.println("########################");
+System.out.println("#                      #");
+System.out.println("#     -----------      #");
+System.out.println("#     | Welcome |      #");
+System.out.println("#     -----------      #");
+System.out.println("#   ^  ^  ^  ^  ^  ^   #");
+System.out.println("#  / \\// \\/ \\/ \\/ \\/ \\  #");
+System.out.println("#  |-B--T--M--2--1--9-| #");
+System.out.println("#  \\ /\\ /\\ /\\ /\\ /\\ /  #");
+System.out.println("#   v  v  v  v  v  v   #");
+System.out.println("# I snowboard and play soccer #");
+    }
+}
